9934fe05ceecc5d232b75976df4728d8abca1a18
Lea Wong <lea.wong@outlook.com>
2019-11-19 14:53:25 +0100

    Update of Player

--- ProjetMonopoly/ProjetMonopoly/Player.cs
+++ ProjetMonopoly/ProjetMonopoly/Player.cs
@@ -11,20 +11,33 @@ namespace ProjetMonopoly
         private String _name;
         private Dictionary<int, int> _balance;
         private int _position;
-        private List<Cards> _cards;
+        //private List<Cell> _properties;
 
-        public Player(string name, Dictionary<int, int> balance,int position, List<Cards> cards)
+        public Player(string name, Dictionary<int, int> balance,int position)
         {
-            this._name = name;
-            this._balance = balance;
+            foreach(char c in name)
+            {
+                this._name += c;
+            }
+            _balance = new Dictionary<int, int>();
+            Dictionary<int, int>.KeyCollection keycoll = balance.Keys;
+            foreach(int key in keycoll)
+            {
+                _balance[key] = balance[key];
+            }
             this._position = position;
-            this._cards = cards;
+            //this._cards = new List<Cell>();
         }
 
-        public string Name { get; }
-        public Dictionary<int,int> Balance { get; set; }
-        public int Position { get; set; }
-        public List<Cards> Cards { get; set; }
+        public string Name { get { return _name; } set { _name = value; } }
+        public Dictionary<int,int> Balance { get { return _balance; } set { _balance = value; } }
+        public int Position { get { return _position; } set { _position = value; } }
+        //public List<Cell> Properties { get { return _properties; } set { _properties = value; } }
 
+        public override string ToString()
+        {
+            string result=  string.Format("Le joueur {0} est en position {1}",Name,Position);
+            return result;
+        }
     }
 }
